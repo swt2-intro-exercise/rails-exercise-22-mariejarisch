a828767b0fa072ac169ce4c049f50272834843f4
mariejarisch <marie.jarisch@student.hpi.uni-potsdam.de>
2022-11-14 13:03:27 +0100

    fail validation of papers created without all attributes and papers with year of non-numerical value

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "Paper model", type: :model do
+  
+    it "should fail validation when a new paper is created without having a title" do
+      paper = Paper.new(title: "", venue: "HPI", year: 2020)
+      expect(paper).to_not be_valid
+    end
+  
+    it "should fail validation when a new paper is created without having a venue" do
+      paper = Paper.new(title: "Skalierbare Softwareentwicklung", venue: "", year: 2020)
+      expect(paper).to_not be_valid
+    end
+
+    it "should fail validation when a new paper is created without having a year" do
+        paper = Paper.new(title: "Skalierbare Softwareentwicklung", venue: "HPI", year: "")
+		    expect(paper).to_not be_valid
+	  end
+
+    it "should fail validation when a new paper is created with year not being a numerical value" do
+      paper = Paper.new(title: "Skalierbare Softwareentwicklung", venue: "HPI", year: "x")
+      expect(paper).to_not be_valid
+  end
 end
