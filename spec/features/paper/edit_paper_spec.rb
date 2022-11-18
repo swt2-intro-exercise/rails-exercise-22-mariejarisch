946347ec5140e1d5eda6a61238a414a17d0aa43d
mariejarisch <marie.jarisch@student.hpi.uni-potsdam.de>
2022-11-18 23:27:48 +0100

    2nd try

--- spec/features/paper/edit_paper_spec.rb
+++ spec/features/paper/edit_paper_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
+describe "Edit paper page", type: :feature do
+    it "should allow the selection of multiple authors" do
+        @paper = FactoryBot.create :paper
+        visit edit_paper_path(@paper)
+        expect(page).to have_css('select[multiple]')
+    end
+end
+
 #describe "Edit paper page", type: :feature do
 #	it "should save changes made to a paper" do
 #		@paper1 = FactoryBot.create :paper
